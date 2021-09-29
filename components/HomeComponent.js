2cafe6d9539fbde1de14cd3e329e0302e2e11cb8
MichaelWhitt <michaelwhitt90@gmail.com>
2021-09-29 11:10:42 -0500

    add cards to home screen

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+//ScrollView can be used to render groups of lists of items (similar to flatlist). 
+//SV loads all at once, Flatlist -> lazy loading which renders part of a list at a time, 
+//on screen or abt to be, parts that have scrolled far off scrn are removed from memory.
+//Lazy Loading is a more efficient way, esp with long lists
+import {Card} from 'react-native-elements';
+import {CAMPSITES} from '../shared/campsites';
+import {PROMOTIONS} from '../shared/promotions';
+import {PARTNERS} from '../shared/partners';
 
+function RenderItem({item}){
+    if(item){
+        return (
+            <Card 
+            featuredTitle={item.name}
+            image={require('./images/react-lake.jpg')}>
+                <Text
+                    style={{margin: 10}}>
+                    {item.description}
+                </Text>
+            </Card>
+        )
+    }
+    return <View/>
+}
 class Home extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            campsites: CAMPSITES,
+            promotions: PROMOTIONS,
+            partners: PARTNERS
+        }
+    }
 
     static navigationOptions = {
         title: 'Home'
@@ -9,9 +41,11 @@ class Home extends Component {
 
     render() {
         return (
-            <View>
-                <Text>Home Component</Text>
-            </View>
+            <ScrollView>
+                <RenderItem item={this.state.campsites.filter(campsite => campsite.featured)[0]}/>
+                <RenderItem item={this.state.promotions.filter(promotion => promotion.featured)[0]}/>
+                <RenderItem item={this.state.partners.filter(partner => partner.featured)[0]}/>
+            </ScrollView>
         );
     }
 }
