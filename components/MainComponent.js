c2f538fddc47c87c96514b0a963cdc185889ee3b
MichaelWhitt <michaelwhitt90@gmail.com>
2021-09-29 10:58:29 -0500

    add drawer nav

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,20 +1,23 @@
-import React, {Component} from "react";
+import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
-import { createStackNavigator } from "react-navigation-stack";
-import { createAppContainer } from "react-navigation";
-import CampsiteInfo from "./CampsiteInfoComponent";
-import { View, Platform } from "react-native";
-import Constants from "expo-constants";
+import CampsiteInfo from './CampsiteInfoComponent';
+import Constants from 'expo-constants';
+import { View, Platform } from 'react-native';
 //Platform import can be used to get the device OS
+import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createAppContainer } from 'react-navigation';
 
+const DirectoryNavigator = createStackNavigator(
 //createStackNavigator takes one mandatory argument called the "Route configs" object
 //this is where you set which components will be available in the stack
-const DirectoryNavigator = createStackNavigator(
+//a second, optional arg is initial route name which defaults the user to whichever component you want
     {
         Directory: { screen: Directory },
         CampsiteInfo: { screen: CampsiteInfo }
-    }, 
-    { //a second, optional arg is initial route name which defaults the user to whichever component you want
+    },
+    {
         initialRouteName: 'Directory',
         defaultNavigationOptions: {
             headerStyle: {
@@ -28,21 +31,48 @@ const DirectoryNavigator = createStackNavigator(
     }
 );
 
+const HomeNavigator = createStackNavigator(
+    {
+        Home: { screen: Home }
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: { screen: HomeNavigator },
+        Directory: { screen: DirectoryNavigator }
+    },
+    {
+        drawerBackgroundColor: '#CEC8FF'
+    }
+);
+
 //createAppContainer returns a react component that handles connecting top level navigator to the react native nav env
 //always wrap top-level nav for the app with createAppContainer, like this:
-const AppNavigator = createAppContainer(DirectoryNavigator);
+const AppNavigator = createAppContainer(MainNavigator);
 //DirectoryNavigator contains screens for both directory and campsite info components
 
-// Conditional code in View style below, check if ios OS, padding 0, else, constants.statusbarheight etc
 class Main extends Component {
+    // first arg: contains screens that will be in the drawer navigator
+    // second (optional) arg:
     render() {
         return (
-            <View
-                style={{
-                    flex: 1,
-                    paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
+            <View style={{
+                flex: 1,
+                paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
             }}>
-                <AppNavigator /> 
+                <AppNavigator />
             </View>
         );
     }
